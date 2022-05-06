34c90c7155e50bee99afc58e14b685f30dd7d087
xero78 <105047389+xero78@users.noreply.github.com>
2022-05-06 14:57:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const btn = document
+    .querySelector('.read-more-btn');
+
+const text = document
+    .querySelector('.card__read-more');
+
+const cardHolder = document
+    .querySelector('.card-holder');
+
+cardHolder
+    .addEventListener('click', e => {
+
+        const current = e.target;
+
+        const isReadMoreBtn = current.className.includes('read-more-btn');
+
+        if (!isReadMoreBtn)
+            return;
+
+        const currentText = e.target.parentNode.querySelector('.card__read-more');
+
+        currentText.classList.toggle('card__read-more--open');
+
+        current.textContent = current.textContent.includes('Read More...') ? 'Read Less...' : 'Read More...';
+
+    });
+
+
+
+    $(".carousel").owlCarousel({
+      loop: true,
+      margin: 25,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      nav: true,
+      navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      }
+    })
+  
